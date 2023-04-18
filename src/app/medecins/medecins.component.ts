ede4e3c07d8dfc81b268490b030029e3ce5ea124
Hexagoneur <115691284+Hexagoneur@users.noreply.github.com>
2023-04-18 12:58:14 +0200

    Add files via upload

--- src/app/medecins/medecins.component.ts
+++ src/app/medecins/medecins.component.ts
@@ -1,75 +1,98 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/app.service.data';
-import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-medecins',
   templateUrl: './medecins.component.html',
   styleUrls: ['./medecins.component.scss']
 })
-@Injectable({
-  providedIn : 'root'
-})
-export class MedecinsComponent  implements OnInit{
-   legende : string = "Rechercher le médecin";
-   estCacheMenu : Boolean = false;
-   nomMedecin : any;
-   medecin : any;
-   lesMedecins: Array<any> = new Array;
-   lesRapports : Array<any>= new Array;
-   afficherRapports : boolean = false;
-   afficherMedecin : boolean = false;
-   afficherMessage : boolean = false;
-   lblMessage="";
-   majMedecin:any;
-   departement:any;
-   
+export class MedecinsComponent implements OnInit {
+  // Propriétés
+  legende = 'Rechercher le médecin';
+  estCacheMenu = false;
+  nomMedecin: any;
+  medecin: any;
+  lesMedecins: Array<any> = [];
+  lesRapports: Array<any> = [];
+  afficherRapports = false;
+  afficherMedecin = false;
+  afficherMessage = false;
+  lblMessage = '';
+  departement: any;
+  idMedecin: any;
+  date: any;
+  motif: any;
+  bilan: any;
+  nomvisiteur: any;
+  afficherListe: any;
 
-   idMedecin : any;
-   date:any;
-   motif:any;
-   bilan:any;
-   nomvisiteur:any;
-   afficherListe:any;
+  constructor(private dataService: DataService) {}
 
+  ngOnInit(): void {}
 
-constructor( private dataService : DataService){}
-  ngOnInit(): void {
+  // Charger la liste des médecins correspondants à la recherche
+  charger(): void {
+    this.dataService.chargerMedecins(this.nomMedecin).subscribe({
+      next: (data) => {
+        this.lesMedecins = data;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
   }
 
-charger(){
-  this.dataService.chargerMedecins(this.nomMedecin).subscribe({
-    next: (data)=>{
-      this.lesMedecins = data;
-
-    },
-    error : (error)=>{
-      console.log(error);
-    }
-  })
-}
-
-selectionner(med:any):void {
-  this.medecin=med
-  this.idMedecin=med.id;  
-  this.nomMedecin = med.nom + " " + med.prenom + "; dep : " + med.departement;
- this.charger();
- this.legende ="";
-  this.afficherRapports = true;
-  this.estCacheMenu = false;
-}
-
-derniersRapports(){
-  this.dataService.chargerRapports(this.idMedecin).subscribe({
-    next: (data) => {
-      this.lesRapports=Array.of(data);
-      this.afficherRapports = true;
-    },
-  
-  error: (error) => {
-    console.log(error);
+  // Sélectionner un médecin et afficher ses derniers rapports
+  selectionner(med: any): void {
+    this.medecin = med;
+    this.idMedecin = med.id;
+    this.nomMedecin = med.nom + ' ' + med.prenom + '; dep : ' + med.departement;
+    this.charger();
+    this.legende = '';
+    this.afficherRapports = true;
+    this.estCacheMenu = false;
   }
-})
 
-}
+  // Charger la liste des derniers rapports pour le médecin sélectionné
+  derniersRapports(): void {
+    this.dataService.chargerRapports(this.idMedecin).subscribe({
+      next: (data) => {
+        this.lesRapports = Array.of(data);
+        this.afficherRapports = true;
+        this.afficherMedecin = false;
+        this.afficherMessage = false;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
+  // Activer le mode édition pour le médecin sélectionné
+  majMedecin(): void {
+    this.afficherRapports = false;
+    this.afficherMedecin = true;
+    this.afficherMessage = false;
+  }
+
+  // Enregistrer les modifications sur le médecin sélectionné
+  valider(): void {
+    this.afficherMessage = true;
+    this.dataService
+      .majMedecin(
+        this.medecin.id,
+        this.medecin.adresse,
+        this.medecin.tel,
+        this.medecin.specialitecomplementaire
+      )
+      .subscribe(
+        (data) => {
+          this.lblMessage = 'Enregistrement effectué';
+        },
+        (error) => {
+          this.lblMessage = 'Merci de réessayer plus tard';
+          console.log(error);
+        }
+      );
+  }
 }
--- src/app/services/app.service.data.ts
+++ src/app/services/app.service.data.ts
@@ -1,37 +1,48 @@
-import { HttpClient, HttpResponse } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable, throwError } from "rxjs";
-import { catchError, retry } from "rxjs/operators";
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: "root",
 })
 export class DataService {
-  private urlAPI = "http://localhost/restGSB";
-  visiteur: any;
+  private urlAPI = "http://localhost/restGSB"; //URL de l'API
 
   constructor(private http: HttpClient) {}
 
+  // Fonction de connexion d'un utilisateur
   public connexion(login: string, mdp: string) {
-    let url = this.urlAPI + "/connexion?login=" + login + "&mdp=" + mdp;
-    let req = this.http.get<string>(url);
-    console.log(url)
-    return req;
-  }
-  public chargerMedecins(nomMedecin : string){
-    let url = this.urlAPI + "/medecins?nom=" + nomMedecin;
-    let req = this.http.get<Array<any>>(url);
-    return req;
-  }
-  public chargerRapports(idMedecin : any){
-    let url = this.urlAPI + "/rapport/" + idMedecin;
-    let req = this.http.get<Array<any>>(url);
-    return req;
-  }
-  public majMedecin(id: string, adresse: string, tel: string, spe: string){
-    let url = this.urlAPI + "/majMedecin" + id + adresse + tel + spe;
-    let req = this.http.get<Array<any>>(url);
-    return req;
+    let url = this.urlAPI + "/connexion?login=" + login + "&mdp=" + mdp; // URL pour appeler la fonction de connexion de l'API
+    let req = this.http.get<string>(url); // Appel à l'API avec une requête HTTP de type GET
+    console.log(url); // Affichage de l'URL appelée dans la console
+    return req; // Retourne la réponse de l'API sous forme d'un Observable de type string
   }
 
+  // Fonction pour charger les médecins en fonction d'un nom
+  public chargerMedecins(nomMedecin: string) {
+    let url = this.urlAPI + "/medecins?nom=" + nomMedecin; // URL pour appeler la fonction de chargement des médecins de l'API
+    let req = this.http.get<Array<any>>(url); // Appel à l'API avec une requête HTTP de type GET
+    return req; // Retourne la réponse de l'API sous forme d'un Observable de type tableau de n'importe quel type
+  }
+
+  // Fonction pour charger les rapports en fonction de l'id du médecin
+  public chargerRapports(idMedecin: any) {
+    let url = this.urlAPI + "/rapport/" + idMedecin; // URL pour appeler la fonction de chargement des rapports de l'API
+    let req = this.http.get<Array<any>>(url); // Appel à l'API avec une requête HTTP de type GET
+    return req; // Retourne la réponse de l'API sous forme d'un Observable de type tableau de n'importe quel type
+  }
+
+  // Fonction pour mettre à jour les informations d'un médecin
+  public majMedecin(id: string, adresse: string, tel: string, spe: string): Observable<any> {
+    const body = { // Création d'un objet qui contient les informations à mettre à jour
+      id: id,
+      adresse: adresse,
+      tel: tel,
+      specialite: spe,
+    };
+    let url: string = this.urlAPI + "/majMedecin"; // URL pour appeler la fonction de mise à jour des informations d'un médecin de l'API
+    let req = this.http.put<Array<any>>(url, body); // Appel à l'API avec une requête HTTP de type PUT pour mettre à jour les informations d'un médecin
+    return req; // Retourne la réponse de l'API sous forme d'un Observable de n'importe quel type
+  }
 }
